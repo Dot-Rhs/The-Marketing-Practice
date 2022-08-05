a4844911ef79ac4b692fd583afc5087a3dfd2b9e
RhysH <rhys.handley@santander.co.uk>
2022-08-05 22:31:24 +0100

    Johnny did good I think
    ## index.html
    - Changed and removed some unused classes
    - Changed layout of footer
    - Increased footer logo size
    - Added div to swiper
    - Moved Navigation button outside of swiper container so there is no container clipping
    
    ## swiper.js
    - Added autoplay to make it clear its swipable in mobile
    
    ## _carousel.scss
    - Moved nested navigation classes outside of .swiper
    - Changed breakpoint size to hide button after tablet view
    
    ## _footer.scss
    - Simplified some repeated classes
    - Removed redundant css
    - Adjusted breakpoints
    - Now desktop > mobile/tab

--- swiper.js
+++ swiper.js
@@ -1,27 +1,37 @@
-var swiper = new Swiper(".mySwiper", {
-    // Default parameters
+var swiper = new Swiper('.mySwiper', {
+	// Default parameters
 
-    //screens less than 768px
-    slidesPerView: 1,
-    centeredSlides: true,
-    loop: true,
-    direction: "horizontal",
-    spaceBetween: 15,
-    breakpoints: {
-        //Screens from 768px
-        768: {
-            slidesPerView: 1.3,
-            spaceBetween: 20,
-        },
-        //screens from 992px
-        992: {
-            slidesPerView: 2.5,
-            spaceBetween: 30,
-            centeredSlides: false,
-
-        }
+	// screens less than 768px
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  direction: 'horizontal',
+  spaceBetween: 15,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: true
+  },
+  speed: 1000,
+  breakpoints: {
+		// Screens from 768px
+    768: {
+      slidesPerView: 1.3,
+      spaceBetween: 20,
+      navigation: {
+        enabled: false
+      }
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-    },
-  });
+		// screens from 992px
+    992: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+      centeredSlides: false,
+      navigation: {
+        enabled: true
+      }
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next'
+  }
+});
